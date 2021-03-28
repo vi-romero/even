87ca742408cc00f9d538a832351eb9db87fb5b97
vi-romero <80266580+vi-romero@users.noreply.github.com>
2021-03-28 11:53:18 +0530

    Create positive.py
    program to print the positive nos. in the list

---
+++ positive.py
@@ -0,0 +1,14 @@
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+
+for num in list1:
+   
+    if num >= 0:
+       print(num, end = " ")
+       
+print("\n")
+
+for num in list2:
+
+       if num >= 0:
+          print(num, end = " ") 
